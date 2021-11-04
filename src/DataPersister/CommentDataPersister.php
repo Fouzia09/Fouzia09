c731f694406c7eccd4ddfc7dda604abc079b12eb
unknown <emery.agossou@y-nissy.com>
2021-11-04 13:41:17 +0100

    gestion des acces pour comment

---
+++ src/DataPersister/CommentDataPersister.php
@@ -0,0 +1,75 @@
+<?php
+
+// src/DataPersister
+
+namespace App\DataPersister;
+
+use App\Entity\Comment;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
+
+/**
+ *
+ */
+class CommentDataPersister implements ContextAwareDataPersisterInterface
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $_entityManager;
+    private $_slugger;
+
+    /**
+     * @param Security
+     */
+    private $_security;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        SluggerInterface $slugger,
+        RequestStack $request,
+        Security $security
+    ) {
+        $this->_entityManager = $entityManager;
+        $this->_slugger = $slugger;
+        $this->_request = $request->getCurrentRequest();
+        $this->_security = $security;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports($data, array $context = []): bool
+    {
+        return $data instanceof Comment;
+    }
+
+    /**
+     * @param Comment $data
+     */
+    public function persist($data, array $context = [])
+    {
+
+        // Set the author if it's a new comment
+        /* if ($this->_request->getMethod() === 'POST') {
+            $data->setAuthor($this->_security->getUser());
+        } */
+
+        // Set the createdAt value if it's not a POST request
+        if ($this->_request->getMethod() !== 'POST') {
+            $data->setCreatedAt(new \DateTime());
+        }
+
+        $this->_entityManager->persist($data);
+        $this->_entityManager->flush();
+    }
+
+    public function remove($data, array $context = [])
+    {
+        $this->_entityManager->remove($data);
+        $this->_entityManager->flush();
+    }
+}
