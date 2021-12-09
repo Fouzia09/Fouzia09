2ea8e7328cc980bc045e13140e057177640715d6
FOUZIA HAJ <hfouzia@hotmail.fr>
2021-12-09 21:24:22 +0100

    modif is_granted

--- src/Entity/Favorite.php
+++ src/Entity/Favorite.php
@@ -12,7 +12,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 /**
  * @ApiResource(
  *      normalizationContext={"groups"={"favorite:read"}},
- *      denormalizationContext={"groups"={"favorite:write"}}
+ *      denormalizationContext={"groups"={"favorite:write"}},
  *      collectionOperations={
  *         "get",
  *         "post"={"security"="is_granted('ROLE_USER')"}
