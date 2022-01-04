0e35762cae8e8d516027c77ad8519b21af3fae6b
GradiM <gradi.mandambu@outlook.com>
2022-01-04 23:22:06 +0100

    Récupération des commentaires liés à une page

---
+++ src/dto/out/CommentOUT.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\dto\out;
+
+class CommentOUT {
+    /**
+     * @Groups({"comment:read", "restaurant:read", "room:read"})
+     */
+    private $id;
+
+    /**
+     * @Groups({"comment:read", "comment:write", "restaurant:read", "room:read"})
+     */
+    private $author;
+
+    /**
+     * @Groups({"comment:read", "comment:write", "restaurant:read", "room:read"})
+     */
+    private $content;
+
+    /**
+     * @Groups({"comment:read", "comment:write", "restaurant:read", "room:read"})
+     */
+    private $createdAt;
+
+    public function __construct(int $id, string $author, string $content, \DateTimeInterface $createdAt) {
+        $this->id = $id;
+        $this->author = $author;
+        $this->content = $content;
+        $this->createdAt = $createdAt;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
