db00c9855f04516e43c44d24cf59df5f7a2ea2da
agoss97 <emeryagossou3@gmail.com>
2022-01-05 01:30:51 +0100

    Push

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -6,9 +6,9 @@ use App\Repository\RoomRepository;
 use App\Service\RoomService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * @Route("/api/rooms")
